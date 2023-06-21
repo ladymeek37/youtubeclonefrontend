8b1f68688134276333ef716f57a473b0d415bf0a
Lady Meek <ladyjmeek@gmail.com>
2023-06-20 18:04:27 -0600

    added proxy url to urlHost

--- src/urlHost.js
+++ src/urlHost.js
@@ -1,4 +1,4 @@
 export const URL_HOST =
   process.env.NODE_ENV === "production"
-    ? "PRODUCTION BASE URL HERE"
+    ? "https://ladys-proxy.ladyjmeek.workers.dev/?targetUrl=http://youtubeclonebackend-env.eba-v3pg8b35.us-east-2.elasticbeanstalk.com"
     : "http://127.0.0.1:8000";
